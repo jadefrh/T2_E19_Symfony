795f5e7c4a13f8200e942c33f5f524fee42e726e
astronaught <jadefrh@gmail.com>
2017-02-13 04:42:05 +0100

    Show entity creation + display on homepage

---
+++ src/AppBundle/Controller/TVShowController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\TVShow;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Student controller.
+ *
+ * @Route("/")
+ */
+class TVShowController extends Controller
+{
+
+    /**
+     * @Route("/show/{id}")
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $shows = $em->getRepository('AppBundle:TVShow')->findAll();
+        return $this->render('AppBundle:TVShow:show.html.twig', array(
+            'shows' => $shows,
+        ));
+    }
+
+}
