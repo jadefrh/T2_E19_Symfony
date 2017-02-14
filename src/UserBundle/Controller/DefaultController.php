27ebfb0831087ef13cf27438dcb0906493f0abd5
astronaught <jadefrh@gmail.com>
2017-02-14 02:37:07 +0100

    progress updated

--- src/UserBundle/Controller/DefaultController.php
+++ src/UserBundle/Controller/DefaultController.php
@@ -1,32 +1,24 @@
 <?php
 
-namespace AppBundle\Controller;
+namespace UserBundle\Controller;
 
-use AppBundle\Entity\User;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 /**
- * login controller.
- *
- * @Route("login")
+ * @Route("login", name="login")
  */
-class UserController extends Controller
+class DefaultController extends Controller
 {
     /**
-     * Lists all chevalier entities.
-     *
      * @Route("/", name="login")
-     * @Method("GET|POST")
      */
     public function indexAction()
     {
         $em = $this->getDoctrine()->getManager();
-
         $users = $em->getRepository('AppBundle:User')->findAll();
 
-        return $this->render('/FOSUSerBundle/login.html.twig', array(
+        return $this->render('layout.html.twig', array(
             'users' => $users,
         ));
     }
