e7c2f4317e69b1d3f2360ba8e17e4f50497f9049
astronaught <jadefrh@gmail.com>
2017-02-14 03:31:24 +0100

    FOSU login template successfully overriden

--- src/UserBundle/Controller/DefaultController.php
+++ src/UserBundle/Controller/DefaultController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 /**
- * @Route("login", name="login")
+ * @Route("user", name="login")
  */
 class DefaultController extends Controller
 {
