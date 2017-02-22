2acd329450e97a96f105285161675c089cdf9497
astronaught <jadefrh@gmail.com>
2017-02-22 19:55:09 +0100

    fixed issues with forms on show page

--- src/AppBundle/Controller/TVShowController.php
+++ src/AppBundle/Controller/TVShowController.php
@@ -7,6 +7,7 @@ use AppBundle\Entity\Vote;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -25,90 +26,67 @@ class TVShowController extends Controller
      */
     public function showAction(Request $request, TVShow $show)
     {
-
-        //FORM WTF
-
-        $voteWTF = new Vote();
-
         $user = $this->get('security.token_storage')->getToken()->getUser();
+        $em = $this->getDoctrine()->getManager();
 
+        // FORM WTF
+        $voteWTF = new Vote();
         $voteWTF->setShow($show);
         $voteWTF->setAuthor($user);
-        $voteWTF->setType('wtf');
 
         $formWTF = $this->createFormBuilder($voteWTF)
+            ->add('type', HiddenType::class)
             ->add('save', SubmitType::class, array('label' => 'WTF'))
             ->getForm();
+
         $formWTF->handleRequest($request);
 
         if ($formWTF->isSubmitted() && $formWTF->isValid()) {
-            // $form->getData() holds the submitted values
-            // but, the original `$task` variable has also been updated
             $voteWTF = $formWTF->getData();
-
-            // ... perform some action, such as saving the task to the database
-            // for example, if Task is a Doctrine entity, save it!
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($voteWTF);
-            $em->flush();
-
-
-
-
+            if ($voteWTF->getType() === 'wtf') {
+                $em->persist($voteWTF);
+                $em->flush();
+            }
         }
 
         //FORM LIKE
-
         $voteLike = new Vote();
-
-        $user = $this->get('security.token_storage')->getToken()->getUser();
-
         $voteLike->setShow($show);
         $voteLike->setAuthor($user);
-        $voteLike->setType('fav');
 
         $formLike = $this->createFormBuilder($voteLike)
-            ->add('save', SubmitType::class, array('label' => 'fav'))
+            ->add('type', HiddenType::class)
+            ->add('save', SubmitType::class, array('label' => 'FAV'))
             ->getForm();
+
         $formLike->handleRequest($request);
 
         if ($formLike->isSubmitted() && $formLike->isValid()) {
-            // $form->getData() holds the submitted values
-            // but, the original `$task` variable has also been updated
             $voteLike = $formLike->getData();
-
-            // ... perform some action, such as saving the task to the database
-            // for example, if Task is a Doctrine entity, save it!
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($voteLike);
-            $em->flush();
-
-
-
-
-
+            if ($voteLike->getType() === 'fav') {
+                $em->persist($voteLike);
+                $em->flush();
+            }
         }
 
-        $showId = $show->getId();
+        $allWtfVotes = $this->getDoctrine()->getRepository('AppBundle:Vote')->findBy([
+            'show' => $show,
+            'type' => 'wtf',
+        ]);
+        $wtfCount = count($allWtfVotes);
 
-        $connection = $em->getConnection();
-        $statementWtf = $connection->prepare("SELECT * FROM vote WHERE show_id = $showId AND type = 'wtf' ");
-        $statementWtf->execute();
-        $statementWtf->fetchAll();
-        $countWtf = $statementWtf->rowCount();
-
-        $connection = $em->getConnection();
-        $statementFav = $connection->prepare("SELECT * FROM vote WHERE show_id = $showId AND type = 'fav' ");
-        $statementFav->execute();
-        $statementFav->fetchAll();
-        $countFav = $statementFav->rowCount();
+        $allFavVotes = $this->getDoctrine()->getRepository('AppBundle:Vote')->findBy([
+            'show' => $show,
+            'type' => 'fav',
+        ]);
+        $favCount = count($allFavVotes);
 
         return $this->render('AppBundle:TVShow:show.html.twig', array(
             'show' => $show,
             'formWTF' => $formWTF->createView(),
             'formLike' => $formLike->createView(),
-            'countFav' => $countFav,
-            'countWtf' => $countWtf,
+            'countFav' => $favCount,
+            'countWtf' => $wtfCount,
         ));
     }
 
