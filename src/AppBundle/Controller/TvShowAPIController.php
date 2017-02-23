454141e3ea7fc64dc7b3e78e66ebfec30bec4c21
annemp <annemauriceperoumal@gmail.com>
2017-02-23 16:12:37 +0100

    apidoc in progress

--- src/AppBundle/Controller/TvShowAPIController.php
+++ src/AppBundle/Controller/TvShowAPIController.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 
 class TvShowAPIController extends Controller
 {
@@ -17,6 +18,23 @@ class TvShowAPIController extends Controller
      * @Route("/TvShowAPIs", name="TvShowAPIs_list")
      * @Method({"GET"})
      */
+
+    /**
+     * @ApiDoc(
+     *      input={
+     *          "class"="FunPro\PassengerBundle\Form\PassengerType",
+     *          "data"={
+     *              "class"="FunPro\PassengerBundle\Entity\Passenger",
+     *              "groups"={"Registeration"},
+     *              "parsers"={
+     *                  "Nelmio\ApiDocBundle\Parser\ValidationParser",
+     *                  "Nelmio\ApiDocBundle\Parser\JmsMetadataParser",
+     *              },
+     *          },
+     *      },
+     * )
+     */
+
     public function getTvShowAPIsAction(Request $request)
     {
         $TvShowAPIs = $this->get('doctrine.orm.entity_manager')
