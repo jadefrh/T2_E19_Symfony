98e7a81e0d80169a511f5038e17ef4cdc364e98e
astronaught <jadefrh@gmail.com>
2017-02-23 17:20:09 +0100

    added wtf and like counts on dashboard

--- src/UserBundle/Controller/UsersController.php
+++ src/UserBundle/Controller/UsersController.php
@@ -8,6 +8,7 @@
 
 namespace UserBundle\Controller;
 
+use AppBundle\Entity\TVShow;
 use AppBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -23,15 +24,31 @@ class UsersController extends Controller
 
         $entities = $em->getRepository('AppBundle:User')->findAll();
         $shows = $em->getRepository('AppBundle:TVShow')->findAll();
+//        dump($shows);exit;
+
+//        foreach($shows as $show){
+//            $favVotes = $this->getDoctrine()->getRepository('AppBundle:Vote')->findBy([
+//                'show' => $show,
+//                'type' => 'fav',
+//            ]);
+//            $wtfVotes = $this->getDoctrine()->getRepository('AppBundle:Vote')->findBy([
+//                'show' => $show,
+//                'type' => 'wtf',
+//            ]);
+//            $wtfCount = count($wtfVotes);
+//            $favCount = count($favVotes);
+//            dump($wtfCount);exit;
+//        }
+
+
 
         return $this->render('@FOSUser/Profile/list.html.twig', array(
             'entities' => $entities,
             'shows' => $shows,
+//            'show' => $show,
+//            'favCount' => $favCount,
+//            'wtfCount' => $wtfCount,
         ));
-
-
-
-
 
     }
 
